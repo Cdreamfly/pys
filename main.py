42318ca8da16c227ed2dfe73fe9984d004e07dca
Cdreamfly <2290660986@qq.com>
2022-05-26 18:11:59 +0800

    实现了日期的自动录入，修复有时不能录入的问题

--- main.py
+++ main.py
@@ -3,33 +3,36 @@
 # 联系电话：就不联系了
 # 声明：此代码仅作为分享交流所用，不许未经作者同意作为商业盈利
 # ###
+
 from selenium import webdriver
 from msedge.selenium_tools import Edge, EdgeOptions
 from selenium.webdriver import ActionChains
 from selenium.webdriver.support.select import Select
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+
 from time import sleep
 import pandas as pd
 
 def Select(id,xpath):
+    driver.find_element_by_id(id).click()
     driver.find_element_by_id(id).find_element_by_xpath(xpath).click()
 
 options = EdgeOptions()
 options.use_chromium = True
 options.binary_location = r"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" # 浏览器的位置
 driver = Edge(options=options, executable_path=r"C:\Users\Cmf\Downloads\edgedriver_win64\msedgedriver.exe") # 相应的浏览器的驱动位置
-driver.get("http://zjt.icitpower.com/pc/Console/Login.aspx")
-driver.maximize_window()
+driver.get("http://zjt.icitpower.com/pc/Console/Login.aspx")#打开地址网页
+driver.maximize_window()#最大化网页
 ## login
-driver.find_element_by_id("txtUserName").click()
-driver.find_element_by_id("txtUserName").send_keys("17550099008")#账号
-driver.find_element_by_id("txtUserPwd").click()
+driver.find_element_by_id("txtUserName").send_keys("17337042333")#账号
 driver.find_element_by_id("txtUserPwd").send_keys("111111")#密码
 driver.find_element_by_id("btnLogin").click()
 
 
 df = pd.read_excel("data.xlsx",index_col=0)
 
-for num in range(500,600):
+for num in range(1,300):
     row_data = df.loc[[num]].values
     for cel in row_data:
         print(cel)
@@ -45,12 +48,15 @@ for num in range(500,600):
     driver.find_element_by_xpath('//*[@id="menu"]/li[1]/dl/dd[1]/a').click()
     driver.switch_to.frame(driver.find_element_by_id('iframe1'))
     #建筑位置
+    driver.find_element_by_id("ddlDist4").click()
     driver.find_element_by_id("ddlDist4").find_element_by_xpath('//*[@id="ddlDist4"]/option[18]').click()
 
     #详细地址
+    driver.find_element_by_xpath('//*[@id="tbRoomAddress"]').click()
     driver.find_element_by_xpath('//*[@id="tbRoomAddress"]').send_keys(address)
 
     #土地性质
+    driver.find_element_by_id("sel_type").click()
     driver.find_element_by_id("sel_type").find_element_by_xpath('//*[@id="sel_type"]/option[3]').click()
 
     #所在区域
@@ -58,9 +64,11 @@ for num in range(500,600):
 
     #建筑名称
     temp = "房屋"+name
+    driver.find_element_by_xpath('//*[@id="tbName"]')
     driver.find_element_by_xpath('//*[@id="tbName"]').send_keys(temp)
 
     #排查对象
+    driver.find_element_by_id("sel_pc_object").click()
     driver.find_element_by_id("sel_pc_object").find_element_by_xpath('//*[@id="sel_pc_object"]/option[2]').click()
 
     #设计方式
@@ -84,7 +92,8 @@ for num in range(500,600):
     #所有权人
     driver.find_element_by_id("btnAdd1").click()
     driver.switch_to.frame(driver.find_element_by_id("layui-layer-iframe1"))
-    driver.find_element_by_id("tbName").send_keys(name)
+    WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_id("tbName")).send_keys(name)
+    #driver.find_element_by_id("tbName").send_keys(name)
     driver.find_element_by_id("btnSave").click()
 
     driver.switch_to.default_content()
@@ -94,7 +103,9 @@ for num in range(500,600):
     #使用人
     driver.find_element_by_id("btnAdd2").click()
     driver.switch_to.frame(driver.find_element_by_id("layui-layer-iframe2"))
-    driver.find_element_by_id("tbName").send_keys(name)
+
+    WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_id("tbName")).send_keys(name)
+    #driver.find_element_by_id("tbName").send_keys(name)
     driver.find_element_by_id("btnSave").click()
 
     driver.switch_to.default_content()
@@ -134,8 +145,11 @@ for num in range(500,600):
     #签字排查 - 政府工作人员
     btn = driver.find_element_by_xpath('//*[@id="btnAdd3"]')
     driver.execute_script("arguments[0].click();", btn)
+    sleep(0.5)
     driver.switch_to.frame(driver.find_element_by_id("layui-layer-iframe3"))
-    driver.find_element_by_id("tbName").send_keys("程展召")
+
+    WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_id("tbName")).send_keys("程展召")
+    #driver.find_element_by_id("tbName").send_keys("程展召")
     driver.find_element_by_id("tbUnit").send_keys("曹集乡政府")
     driver.find_element_by_id("btnSave").click()
 
@@ -146,8 +160,10 @@ for num in range(500,600):
     #签字排查 - 技术专家
     btn = driver.find_element_by_xpath('//*[@id="btnAdd4"]')
     driver.execute_script("arguments[0].click();", btn)
+    sleep(0.5)
     driver.switch_to.frame(driver.find_element_by_id("layui-layer-iframe4"))
-    driver.find_element_by_id("tbName").send_keys("梁东建")
+    WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_id("tbName")).send_keys("梁东建")
+    #driver.find_element_by_id("tbName").send_keys("梁东建")
     driver.find_element_by_id("tbUnit").send_keys("夏邑县住房保障服务中心")
     driver.find_element_by_id("btnSave").click()
 
@@ -157,8 +173,13 @@ for num in range(500,600):
 
     #排查结论
     driver.find_element_by_id("sel_result").find_element_by_xpath('//*[@id="sel_result"]/option[2]').click()
-
-    sleep(10)
+    #排查时间
+    driver.find_element_by_xpath('//*[@id="tb_pc_date"]').click()
+    driver.switch_to.default_content()
+    iframe = driver.find_element('xpath', '//*[@id="_my97DP"]/iframe')
+    driver.switch_to.frame(iframe)
+    driver.find_element_by_xpath('/html/body/div/div[3]/table/tbody/tr[6]/td[3]').click()
+    driver.switch_to.default_content()
+    driver.switch_to.frame(driver.find_element_by_id("iframe1"))
     #保存
     driver.find_element_by_xpath('//*[@id="btnSave"]').click()
-    sleep(1)
